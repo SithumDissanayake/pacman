0c89f68f8d30cd4019676c0f3db05088264323f4
Sithum Dissanayake <sithumsdissanayake@gmail.com>
2018-11-05 12:38:28 +1100

    update

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(){
-  var theDelay = 1.5;
-  var timer = setTimeout("showText()",theDelay*1000)
-}
-function showText(){
-  document.getElementById("delayedText").style.visibility = "visible";
-}
